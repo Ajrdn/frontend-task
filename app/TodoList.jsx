8c232633ce48b1f39791d02423e43240b4e8f92b
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-09-12 15:12:50 +0900

    UPDATE::store

--- app/TodoList.jsx
+++ app/TodoList.jsx
@@ -1,16 +1,15 @@
-import {React, useContext} from 'react'
+import React from 'react'
 import Todo from './Todo'
-import TodoListContext from './TodoListContext'
+import todoListStore from './TodoListStore'
 
-export default function TodoList({setLogList}) {
-  const todoList = useContext(TodoListContext)
+export default function TodoList() {
+  const todoList = todoListStore(state => state.todoList)
 
   return (
     todoList.map((todo, index) => (
       <Todo
         key={`${todo} + ${index}`}
         todo={todo}
-        setLogList={setLogList}
       />
     ))
   )
